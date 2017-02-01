352dd4e542a419a2ddbeecfd52826331e9b09484
josamanun <josamanun64@gmail.com>
2017-02-01 23:16:32 +0100

    Integrando el plugin Taglist

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Ejercicio con maven en el que se integran los siguientes plugins:
 
 * Integración del plugin pmd.
+Taglist. Genera un documento HTML con una lista de cosas por hacer, la cual toma etiquetas como TODO y FIXME para generar el documento. Se ejecuta cuando hacemos: mvn site
 
 
 Jose Antonio Amaya Nuñez.
--- src/main/java/org/egc/App.java
+++ src/main/java/org/egc/App.java
@@ -8,10 +8,12 @@ public class App
 {
 	public void bar() {
 		
+        //FIXME: error PMD de variable local no usada
 		/* Unused local variable. */
 		String foo = "";
 		String bar = "";
 
+        //FIXME: error PMD de los bloques try y catch vacios
 		try{
         	/* Empty try blocks */
         }catch(Exception e){
@@ -20,14 +22,17 @@ public class App
 
         if (bar == "foo") bar = "bar";
 
+        //FIXME: error PMD del if
         /* If statement that is always true or always false */
         if (true)
         	bar = "IfStmtsMustUseBraces";
         else
         	bar = "bar";
 	}
+
+    //TODO:metodo para controlar el mundo
 	
     public static void main( String[] args ) {
-        System.out.println("josamanun");
+        System.out.println("alemurrod");
     }
 }
