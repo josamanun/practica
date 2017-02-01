17147a2dd8bf7cc12fb72bc864a0bee300d8659f
josamanun <josamanun64@gmail.com>
2017-02-01 23:30:25 +0100

    Correccion de PMD para Travis

--- src/main/java/org/egc/App.java
+++ src/main/java/org/egc/App.java
@@ -9,25 +9,15 @@ public class App
 		
         //FIXME: error PMD de variable local no usada
 		/* Unused local variable. */
-		String foo = "";
 		String bar = "";
 
         //FIXME: error PMD de los bloques try y catch vacios
-		try{
-        	/* Empty try blocks */
-        }catch(Exception e){
-        	/* Empty catch blocks */
-        }
+		
 
         if (bar == "foo") bar = "bar";
 
         //FIXME: error PMD del if
-        /* If statement that is always true or always false */
-        if (true)
-        	bar = "IfStmtsMustUseBraces";
-        else
-        	bar = "bar";
-	}
+        
 
     //TODO:metodo para controlar el mundo
 	
@@ -37,6 +27,6 @@ public class App
         Logger slf4jLogger = LoggerFactory.getLogger(App.class);
         slf4jLogger.info("Hello world");
 
-        System.out.println("josamanun");
+        System.out.println("alemurrod");
     }
 }
